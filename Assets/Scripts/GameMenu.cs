5a913b16ba726785dd226685c664ac4d17088ed9
David Jiang <djiang86@binghamton.edu>
2022-05-20 23:29:50 -0400

    Opening and Closing Menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject theMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (theMenu.activeInHierarchy)
+            {
+                theMenu.SetActive(false);
+            } else
+            {
+                theMenu.SetActive(true);
+            }
+        }
+    }
+}
