0491f9fbf2ffe3d7f56a636479829b927073de9f
David Jiang <djiang86@binghamton.edu>
2022-05-22 23:23:31 -0400

    Showing the items in menu

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -21,10 +21,21 @@ public class GameMenu : MonoBehaviour
     public Image[] charImages;
     public GameObject[] charStatHolder;
 
-    public Text statusName, statusHP, statusMP, statusStr, statusDef, 
-        statusWpnEqpd, statusWpnPwr, statusArmrEqpd, statusArmrPwr, statusExp;
+    [Header("Player Status")]
+    public Text statusName;
+    public Text statusHP;
+    public Text statusMP;
+    public Text statusStr;
+    public Text statusDef;
+    public Text statusWpnEqpd;
+    public Text statusWpnPwr;
+    public Text statusArmrEqpd;
+    public Text statusArmrPwr;
+    public Text statusExp;
     public Image statusImage;
 
+    public ItemButton[] itemButtons;
+        
     // Start is called before the first frame update
     void Start()
     {
@@ -131,5 +142,24 @@ public class GameMenu : MonoBehaviour
         statusArmrPwr.text = playerStats[selected].armorPower.ToString();
         statusExp.text = (playerStats[selected].expToNextLevel[playerStats[selected].playerLevel] - playerStats[selected].currentExp).ToString();
         statusImage.sprite = playerStats[selected].charImage;
+    }
+
+    public void ShowItems()
+    {
+        for (int i = 0; i < itemButtons.Length; i++)
+        {
+            itemButtons[i].buttonValue = i;
+
+            if (GameManager.instance.itemsHeld[i] != "")
+            {
+                itemButtons[i].buttonImage.gameObject.SetActive(true);
+                itemButtons[i].buttonImage.sprite = GameManager.instance.GetItemDetails(GameManager.instance.itemsHeld[i]).itemSprite;
+                itemButtons[i].amountText.text = GameManager.instance.numberOfItems[i].ToString();
+            } else
+            {
+                itemButtons[i].buttonImage.gameObject.SetActive(false);
+                itemButtons[i].amountText.text = "";
+            }
+        }
     }
 }
